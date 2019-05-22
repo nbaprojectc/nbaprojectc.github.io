04d4276c698659e8381770eb95b2cab7261a5d25
NBAProjectC <nbaprojectc@nxk.io>
2019-05-22 05:18:53 +0000

    site update

---
+++ precache-manifest.7e2e6a55a72752e06fd3f2534d25700d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1ddc5329233a88d200",
+    "url": "/NbaProjectCQA/static/js/runtime~main.1003313a.js"
+  },
+  {
+    "revision": "86415882ff26c3e23487",
+    "url": "/NbaProjectCQA/static/js/main.e7aa422d.chunk.js"
+  },
+  {
+    "revision": "8afdbe9182249f795c70",
+    "url": "/NbaProjectCQA/static/js/2.04271de9.chunk.js"
+  },
+  {
+    "revision": "86415882ff26c3e23487",
+    "url": "/NbaProjectCQA/static/css/main.18b592e3.chunk.css"
+  },
+  {
+    "revision": "8afdbe9182249f795c70",
+    "url": "/NbaProjectCQA/static/css/2.f869753d.chunk.css"
+  },
+  {
+    "revision": "7023c36d3976206129ff472af34c1cc7",
+    "url": "/NbaProjectCQA/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59aa0842cab58ac357b5752b9351da36.js"
+  "/NbaProjectCQA/precache-manifest.7e2e6a55a72752e06fd3f2534d25700d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/NbaProjectCQA/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
